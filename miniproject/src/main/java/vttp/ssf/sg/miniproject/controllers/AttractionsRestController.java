de5a4b303392d5276a7438fc9279e0863e1e5c83
ashleychews <84390323+ashleychews@users.noreply.github.com>
2024-01-01 02:03:54 +0800

    modified:   miniproject/src/main/java/vttp/ssf/sg/miniproject/controllers/AttractionsRestController.java 	modified:   miniproject/src/main/resources/static/attraction.css 	modified:   miniproject/src/main/resources/static/favourite.css 	modified:   miniproject/src/main/resources/static/search.css 	modified:   miniproject/src/main/resources/static/searchresults.css 	modified:   miniproject/src/main/resources/templates/attraction.html 	modified:   miniproject/src/main/resources/templates/favourite.html 	modified:   miniproject/src/main/resources/templates/search.html 	modified:   miniproject/src/main/resources/templates/searchresults.html

--- miniproject/src/main/java/vttp/ssf/sg/miniproject/controllers/AttractionsRestController.java
+++ miniproject/src/main/java/vttp/ssf/sg/miniproject/controllers/AttractionsRestController.java
@@ -1,10 +1,7 @@
 package vttp.ssf.sg.miniproject.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,13 +18,8 @@ public class AttractionsRestController {
     @Autowired
     private AttractionsService attSvc;
 
-    // @GetMapping(path = "/searchAttractions", produces = "application/json")
-    // public ResponseEntity<String> attractionSearch(@RequestParam String uuid) {
-    //     Attractions attraction = attSvc.getAttractionDetailsByUUID(uuid);
-
-
-
-    //     return 
-    // }
-
+    @GetMapping(path = "/searchattractions", produces = "application/json")
+    public Attractions getAttractions(@RequestParam String uuid) {
+        return attSvc.getAttractionDetailsByUUID(uuid);
+    }
 }
